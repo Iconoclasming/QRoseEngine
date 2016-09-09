22d3df2dc05c94e9872b44215587699f30ca78ab
Anton Kanashkin <janeiconoclasm@gmail.com>
2016-09-09 18:03:24 +0700

    Added interesting streaming concept

--- include/QRoseEngine.Core/Streaming.hpp
+++ include/QRoseEngine.Core/Streaming.hpp
@@ -28,4 +28,17 @@ namespace QRose
 		static bool Read(bool& booleanOut, const std::istream& deserializationStream);
 		static bool Read(std::string& strOut, const std::istream& deserializationStream);
 	};
+	
+	/*
+	// When the class Archive corresponds to an output archive, the
+    	// & operator is defined similar to <<.  Likewise, when the class Archive
+	 // is a type of input archive the & operator is defined similar to >>.
+    	template<class Archive>
+	 void serialize(Archive & ar, const unsigned int version)
+    	{
+        	ar & degrees;
+        	ar & minutes;
+        	ar & seconds;
+    	}
+    	*/
 }
